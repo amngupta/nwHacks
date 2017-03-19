3060c778eab0e160bbcb7a5c367e2aba29b07aba
seleena <seleena_hua@hotmail.com>
2017-03-19 02:33:10 -0700

    Changed title page and hide some links to non-logged in users

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './index.css';
 
+
 ReactDOM.render(
   Routes,
   document.getElementById('root')
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import {
     BrowserRouter as Router,
     Route,
@@ -10,28 +10,21 @@ import UserList from './components/Users'
 import CompanySubscriptionList from './components/CompanySubscriptions'
 import {EnsureLoggedInContainer, AuthButton, Login} from './components/EnsureLoggedIn'
 const styles = {
-    menu: { width: '20%', float: 'left'},
+    menu: { width: '20%', float: 'center'},
     menuUl: { listStyle: 'none'},
-    content: { width: '80%', float: 'left'}
+    content: { width: '100%'}
 }
 const Routes = (
     <Router>
-        <div>
-            <div style={styles.menu}>
-                <ul style={styles.menuUl}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/plans">Plans</Link></li>
-                    <li><Link to="/subscribers">Subscribers</Link></li>
-                    <li><AuthButton/></li>
-                </ul>
-            </div>
-            <div style={styles.content}>
-                <Route exact path="/" component={AppContainer} / >
-                <Route path="/login" component={Login}/>
-                <EnsureLoggedInContainer path="/plans" component={CompanySubscriptionList} />
-                <EnsureLoggedInContainer path="/subscribers" component={UserList} />
-            </div>
+    <div>
+    <AuthButton/>      
+        <div style={styles.content}>
+            <Route exact path="/" component={AppContainer} / >
+            <Route path="/login" component={Login}/>
+            <EnsureLoggedInContainer path="/plans" component={CompanySubscriptionList} />
+            <EnsureLoggedInContainer path="/subscribers" component={UserList} />
         </div>
+    </div>
     </Router>
 );
 
