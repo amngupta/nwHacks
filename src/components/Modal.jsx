8e4dcd2497774c833683234d8c3479a6663d62ee
amngupta <amangupta96@outlook.com>
2017-03-18 20:13:37 -0700

    Modal complete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -29,7 +29,7 @@ export class ModalTest extends Component {
     }
 
     close() {
-        this.setState({ showModal: false });
+        this.setState({ page: false, showModal: false });
     }
 
     open() {
@@ -40,13 +40,7 @@ export class ModalTest extends Component {
         const creditCardForm = (
             <div>
                 <CardReactFormContainer
-    
-                // the id of the container element where you want to render the card element.
-                // the card component can be rendered anywhere (doesn't have to be in ReactCardFormContainer).
-                container="card-wrapper" // required
-                
-                // an object contain the form inputs names.
-                // every input must have a unique name prop.
+                container="card-wrapper" // required  
                 formInputsNames={
                     {
                     number: 'CCnumber', // optional — default "number"
@@ -54,28 +48,15 @@ export class ModalTest extends Component {
                     cvc: 'CCcvc', // optional — default "cvc"
                     name: 'CCname' // optional - default "name"
                     }
-                }
-                
-                // initial values to render in the card element
-                initialValues= {
-                    {
-                    number: '•••• •••• •••• ••••', // optional — default •••• •••• •••• ••••
-                    cvc: '•••', // optional — default •••
-                    expiry: '••/••', // optional — default ••/••
-                    name: 'Full Name' // optional — default FULL NAME
-                    }
-                }
+                }   
                   formatting={true} // optional - default true
-
-                
-                // specify whether you want to format the form inputs or not
                 >
                 
                 <form>
-                    <input placeholder="Full name" type="text" name="CCname" className="form-control" /> 
-                    <input placeholder="Card number" type="text" name="CCnumber" className="form-control"/>
-                    <input placeholder="MM/YY" type="text" name="CCexpiry" className="form-control"/>
-                    <input placeholder="CVC" type="text" name="CCcvc" className="form-control"/>
+                    <FormControl placeholder="Full name" type="text" name="CCname" />
+                    <FormControl placeholder="Card number" type="text" name="CCnumber" />
+                    <FormControl placeholder="MM/YY" type="text" name="CCexpiry"/>
+                    <FormControl placeholder="CVC" type="text" name="CCcvc"/>
                 </form>
                 
                 </CardReactFormContainer>
@@ -121,14 +102,14 @@ export class ModalTest extends Component {
                         type="text"
                         placeholder="Address Line 2" />
                     <br />
-                    <div className="form-group col-sm-6">
+                    <div className="col-sm-6">
                         <div className="col-sm-12">
                             <CountryDropdown
                                 value={country}
                                 onChange={(val) => this.selectCountry(val)} classes="form-control" />
                         </div>
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="col-sm-6">
                         <div className="col-sm-12">
                             <RegionDropdown
                                 country={country}
