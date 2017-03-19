463302ee9ce70efb12abfba5020f2ec09322e2fa
brian <brianhylee99@gmail.com>
2017-03-18 19:16:35 -0700

    Implemented credit card form

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Row, Modal, Button, Col, FormControl, FormGroup } from 'react-bootstrap';
 import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
 
+import CardReactFormContainer from 'card-react';
+import './card.css';
+
 export class ModalTest extends Component {
     constructor(props) {
         super(props);
@@ -33,6 +36,61 @@ export class ModalTest extends Component {
         this.setState({ showModal: true });
     }
     render() {
+
+        const creditCardForm = (
+            <div>
+            <CardReactFormContainer
+ 
+        // the id of the container element where you want to render the card element.
+        // the card component can be rendered anywhere (doesn't have to be in ReactCardFormContainer).
+        container="card-wrapper" // required
+        
+        // an object contain the form inputs names.
+        // every input must have a unique name prop.
+        formInputsNames={
+            {
+            number: 'CCnumber', // optional — default "number"
+            expiry: 'CCexpiry',// optional — default "expiry"
+            cvc: 'CCcvc', // optional — default "cvc"
+            name: 'CCname' // optional - default "name"
+            }
+        }
+        
+        // initial values to render in the card element
+        initialValues= {
+            {
+            number: '4242424242424242', // optional — default •••• •••• •••• ••••
+            cvc: '123', // optional — default •••
+            expiry: '16/12', // optional — default ••/••
+            name: 'Random Name' // optional — default FULL NAME
+            }
+        }
+        
+        // the class name attribute to add to the input field and the corresponding part of the card element,
+        // when the input is valid/invalid.
+        classes={
+            {
+            valid: 'valid-input', // optional — default 'jp-card-valid'
+            invalid: 'invalid-input' // optional — default 'jp-card-invalid'
+            }
+        }
+        
+        // specify whether you want to format the form inputs or not
+        formatting={true} // optional - default true
+        >
+        
+        <form>
+            <input placeholder="Full name" type="text" name="CCname" />
+            <input placeholder="Card number" type="text" name="CCnumber" />
+            <input placeholder="MM/YY" type="text" name="CCexpiry" />
+            <input placeholder="CVC" type="text" name="CCcvc" />
+        </form>
+        
+        </CardReactFormContainer>
+        <div id="card-wrapper"></div>
+</div>
+
+        )
 
         const loginButtons = (
             <div>
@@ -66,11 +124,11 @@ export class ModalTest extends Component {
                     />
                     <br />
                     <FormControl
-                        type="address"
+                        type="text"
                         placeholder="Address Line 1" />
                     <br />
                     <FormControl
-                        type="address"
+                        type="text"
                         placeholder="Address Line 2" />
                     <br />
                     <div className="form-group col-sm-6">
@@ -94,7 +152,7 @@ export class ModalTest extends Component {
         let modalBody = null;
         let buttonName = null;
         if (this.state.page) {
-            modalBody = loginButtons;
+            modalBody = creditCardForm;
             buttonName = (<Button >Submit</Button>)
         }
         else {
