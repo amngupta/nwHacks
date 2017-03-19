266e0239ccf2c676d08738f3b075d02071bea0a8
Leanne Zhang <lzhang800@gmail.com>
2017-03-18 22:38:24 -0700

    added subscription list

--- src/components/CompanySubscriptions/CompanySubscriptionList.jsx
+++ src/components/CompanySubscriptions/CompanySubscriptionList.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import Subscription from './SubscriptionItem'
 
-function CompanySubscriptionList(props) {
+const subscriptions = [{
+  id: 1,
+  name: 'Hootsuite',
+  tier: 'Professional',
+  usersNum: 20,
+  features: 'Message scheduling Real-time analytics',
+  cost: '$9.99'
+},{
+  id: 2,
+  name: 'Hootsuite',
+  tier: 'Basic',
+  usersNum: 20,
+  features: 'Custom analytics reports',
+  cost: '$9.99'
+}];
 
-  return (
-    <div>
-      subscription list
-      <Subscription />
+const styles = {
+  subscriptionList: { border: '1px solid #e1e4e8', borderTop: 'none'}
+}
+
+class CompanySubscriptionList extends React.Component {
+
+  render(){
+   return (
+     <div style={styles.subscriptionList}>
+       {
+         subscriptions.map(function(item) {
+          return  <Subscription key={item.id} subscription={item} />
+        })
+       }
     </div>
-  )
+   )
+  }
 }
 
 export default CompanySubscriptionList;
